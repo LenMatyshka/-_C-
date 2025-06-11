2c0fe1deb73059f7c91060cdf325ba3a4c43a895
LenMatyshka <moon-princes15@mail.ru>
2025-06-11 18:51:10 +0300

    Add files via upload

---
+++ 1-2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        Console.Write("введите a = ");
+        int a = int.Parse(Console.ReadLine());
+
+        Console.Write("введите b = ");
+        int b = int.Parse(Console.ReadLine());
+
+        if (b == 0)
+        {
+            Console.WriteLine("Деление на ноль");
+            return;
+        }
+
+        int quotient = 0;
+        int absA = Math.Abs(a);
+        int absB = Math.Abs(b);
+
+        while (absA >= absB)
+        {
+            absA -= absB;
+            quotient++;
+        }
+
+        if ((a < 0 && b > 0) || (a > 0 && b < 0))
+            quotient = -quotient;
+
+        Console.WriteLine($"Неполное частное: {quotient}");
+    }
+}
---
+++ 2-1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+Console.WriteLine("Введите  строку S: ");
+string S = Console.ReadLine();
+Console.WriteLine("Введите строку S1: ");
+string S1 = Console.ReadLine();
+static int Count(string S1, string S)
+{
+    int count = (S.Length - S.Replace(S1, "").Length) / S1.Length;
+    return count;
+}
+Console.WriteLine($"количество вхождений S1 в S: {Count(S1, S)}");
