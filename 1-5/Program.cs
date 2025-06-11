8b3f3c782b0e3c78cddb78ad076ad75f2646c4b7
LenMatyshka <moon-princes15@mail.ru>
2025-06-11 18:48:30 +0300

    Add files via upload

---
+++ 1-5/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите стороны треугольника 1:");
+        int a1 = Convert.ToInt32(Console.ReadLine());
+        int b1 = Convert.ToInt32(Console.ReadLine());
+        int c1 = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Введите стороны треугольника 2 :");
+        int a2 = Convert.ToInt32(Console.ReadLine());
+        int b2 = Convert.ToInt32(Console.ReadLine());
+        int c2 = Convert.ToInt32(Console.ReadLine());
+
+      
+        int max1 = Math.Max(a1, Math.Max(b1, c1));
+        int min1 = Math.Min(a1, Math.Min(b1, c1));
+        int mid1 = a1 + b1 + c1 - max1 - min1;
+
+       
+        int max2 = Math.Max(a2, Math.Max(b2, c2));
+        int min2 = Math.Min(a2, Math.Min(b2, c2));
+        int mid2 = a2 + b2 + c2 - max2 - min2;
+
+    
+        if (max1 * mid2 == max2 * mid1 && mid1 * min2 == mid2 * min1)
+        {
+            Console.WriteLine("Треугольники подобны");
+        }
+        else
+        {
+            Console.WriteLine("Треугольники не подобны");
+        }
+    }
+}
---
+++ 1.4/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        int a, b, c, res, count;
+
+        Console.WriteLine("Введите три числа:");
+
+        Console.WriteLine("первое:");
+        if (!int.TryParse(Console.ReadLine(), out a))
+        {
+            Console.WriteLine("Ошибка: Введите целое число.");
+            return;
+        }
+
+        Console.WriteLine("второе:");
+        if (!int.TryParse(Console.ReadLine(), out b))
+        {
+            Console.WriteLine("Ошибка: Введите целое число.");
+            return;
+        }
+
+        Console.WriteLine("третье:");
+        if (!int.TryParse(Console.ReadLine(), out c))
+        {
+            Console.WriteLine("Ошибка: Введите целое число.");
+            return;
+        }
+
+        res = 0;
+
+        for (int i = 0; i < 32; i++)
+        {
+                     count = ((a >> i) & 1) + ((b >> i) & 1) + ((c >> i) & 1);
+
+                        if (count >= 2)
+            {
+                res |= (1 << i);
+            }
+        }
+
+        Console.WriteLine($"Новое число: {res}");
+    }
+}
